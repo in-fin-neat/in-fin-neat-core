1f40eddb52448605915ec121b48f472620ce09b5
Leonardo Castilho <leonardohscastilho@gmail.com>
2023-08-23 23:32:53 +0200

    Create a unit test case for the data folder creation

---
+++ tests/test_fetch_transactions.py
@@ -0,0 +1,27 @@
+import pytest
+import re
+import io
+import os
+from typing import List
+
+from  personal_finances.fetch_transactions import _ensure_data_path_exist
+
+
+def test_if_data_path_gets_created() -> None:
+    """
+    Change current execution folder to tests to not consider the 'data'
+    folder from the application execution, what could lead to a false positive
+    in the test result
+    """
+    os.chdir('tests')
+    _ensure_data_path_exist()
+    
+    data_path_get_created = os.path.exists('data')
+    assert (data_path_get_created)
+
+    """
+    Cleaning path after testing execution
+    """
+    if data_path_get_created:
+        os.rmdir('data')
+
