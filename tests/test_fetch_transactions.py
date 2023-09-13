22cdd2ee00bd05208bebc31887720fe6b644d95d
Leonardo Castilho <leonardohscastilho@gmail.com>
2023-09-13 21:43:40 +0200

    Update the fetch transactions unit test to mock imported libs
    Update the fetch transactions unit test to mock imported libs (os, bank_client and open) instead to import the real libs

--- tests/test_fetch_transactions.py
+++ tests/test_fetch_transactions.py
@@ -1,21 +1,52 @@
-import os
-from personal_finances.fetch_transactions import _ensure_data_path_exist
+from unittest.mock import mock_open, patch, Mock
+from pytest import fixture
+from click.testing import CliRunner
+from typing import Generator
+from personal_finances.fetch_transactions import fetch_transactions
 
 
-def test_if_data_path_gets_created() -> None:
-    """
-    Change current execution folder to tests to not consider the 'data'
-    folder from the application execution, what could lead to a false positive
-    in the test result
-    """
-    os.chdir("tests")
-    _ensure_data_path_exist()
+@fixture()
+def os_mock() -> Generator[Mock, None, None]:
+    with patch("personal_finances.fetch_transactions.os") as mock:
+        yield mock
 
-    data_path_get_created = os.path.exists("data")
-    assert data_path_get_created
 
+@fixture(autouse=True)
+def bank_client_mock() -> Generator[Mock, None, None]:
+    with patch("personal_finances.fetch_transactions.BankClient") as mock:
+        yield mock
+
+
+@fixture(autouse=True)
+def open_mock() -> Generator[Mock, None, None]:
+    m = mock_open()
+    with patch("__main__.open", m):
+        yield m
+
+
+def test_data_path_not_created_if_already_exists(os_mock: Mock) -> None:
+    """ 
+    Mock to return like the data path already exists.
+    At this way, the function should not try to create a path for it.
     """
-    Cleaning path after testing execution
+    os_mock.path.exists.return_value = True
+
+    runner = CliRunner()
+    runner.invoke(fetch_transactions, [])
+
+    os_mock.path.exists.assert_called_once_with("data")
+    os_mock.makedirs.assert_not_called()
+
+
+def test_data_path_is_created_if_not_exists(os_mock: Mock) -> None:
+    """ 
+    Mock to return like the data path has yet to be created. 
+    In this way, the function should create it.
     """
-    if data_path_get_created:
-        os.rmdir("data")
+    os_mock.path.exists.return_value = False
+
+    runner = CliRunner()
+    runner.invoke(fetch_transactions, [])
+
+    os_mock.path.exists.assert_called_once_with("data")
+    os_mock.makedirs.assert_called_once_with("data")
