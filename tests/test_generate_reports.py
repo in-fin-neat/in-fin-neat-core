7a3e4ee8b2f36acda4d110229f629c0e99a5d415
Leonardo Castilho <leonardohscastilho@gmail.com>
2024-03-27 22:39:07 +0100

    Init of generate reports

--- tests/test_generate_reports.py
+++ tests/test_generate_reports.py
@@ -1,11 +1,24 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, mock_open
 import pytest
+from pytest import fixture
 from click.testing import CliRunner
 from personal_finances.generate_reports import generate_reports
 
+transactions_file = '{"test" : "teste" }'
+
+
+@fixture(autouse=True)
+def open_mock() -> Generator[Mock, None, None]:
+    m = mock_open(read_data = transactions_file)()
+    with patch("personal_finances.fetch_transactions.open", m):
+        yield m
+
 
 def test_malformed_time_interval_is_rejected() -> None:
-    # TODO: test start and end times
+    # TODO:: invalid string format test
+    runner = CliRunner()
+    result = runner.invoke(generate_reports, ['--st 1971-01-01T00:00:00Z --et 1970-01-01T00:00:00Z'])
+    assert result.exception == ValueError
     pass
 
 
@@ -13,8 +26,32 @@ def test_reads_correct_input_transaction_file() -> None:
     # TODO: test default and custom file
     # TODO: test file path extension is json
     # TODO: test malformed json content is rejected
-    pass
-
+    with patch("personal_finances.generate_reports.open") as open_mock:
+        runner = CliRunner()
+        result = runner.invoke(generate_reports, [])
+        open_mock.path.exists.assert_called_once_with("config/user_config.yaml")
+        assert result.exit_code == 0
+    
+    with patch("personal_finances.generate_reports.open") as open_mock:
+        runner = CliRunner()
+        result = runner.invoke(generate_reports, ['-tfp test_file_path'])
+        open_mock.path.exists.assert_called_once_with("test_file_path")
+        assert result.exit_code == 0
+    
+    open_mock = mock_open(read_data = "this is not a json file")()
+    with patch("personal_finances.generate_reports.open", open_mock):
+        runner = CliRunner()
+        result = runner.invoke(generate_reports, [])
+        open_mock.path.exists.assert_called_once_with("test_file_path")
+        assert result.exception
+        
+        open_mock = mock_open(read_data = '{ "malformed": json }')()
+    with patch("personal_finances.generate_reports.open", open_mock):
+        runner = CliRunner()
+        result = runner.invoke(generate_reports, [])
+        open_mock.path.exists.assert_called_once_with("test_file_path")
+        assert result.exception
+        
 
 def test_reads_correct_input_user_config_file() -> None:
     # TODO: test default and custom file
