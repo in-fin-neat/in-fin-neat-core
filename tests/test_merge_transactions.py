c9370badb3bffefecd30a10db7482abf10920895
Leonardo Castilho <leonardohscastilho@gmail.com>
2024-04-17 23:26:37 +0200

    Update unittests with last comments from PR

--- tests/test_merge_transactions.py
+++ tests/test_merge_transactions.py
@@ -1,6 +1,6 @@
 from pytest import fixture
 from unittest.mock import patch, Mock, mock_open
-from typing import Generator, List, Optional, Union
+from typing import Generator, List
 from click.testing import CliRunner
 import pytest
 from personal_finances.bank_interface.nordigen_adapter import (
@@ -31,118 +31,204 @@ def open_mock() -> Generator[Mock, None, None]:
         yield m
 
 
-def create_nordigen_transaction(
-    amount: float, id: Optional[str] = None, internal_id: Optional[str] = None
+@fixture(autouse=True)
+def datetime_mock() -> Generator[Mock, None, None]:
+    with patch("personal_finances.merge_transactions.datetime") as mock:
+        yield mock
+
+
+def new_transaction(
+    amount: float,
+    transactionId: str,
+    internalTransactionId: str,
+    currency: str,
 ) -> NordigenTransaction:
-    params: dict = {
-        "bookingDate": "dummy_data",
-        "bookingDatetime": "dummy_data",
-        "transactionAmount": TransactionAmount(currency="EUR", amount=amount),
-    }
-
-    if internal_id is not None:
-        params["internalTransactionId"] = internal_id  # Adjust the key if provided
-
-    if id is not None:
-        params["transactionId"] = id  # Adjust the key if provided
-
-    return NordigenTransaction(**params)
+    return NordigenTransaction(
+        bookingDate="2021-01-01T10:00:00.00000",
+        bookingDatetime="2021-01-01T10:00:00.00000",
+        transactionAmount=TransactionAmount(currency=currency, amount=amount),
+        transactionId=transactionId,
+        internalTransactionId=internalTransactionId,
+    )
 
 
-TEST_SAME_ID_TRANSACTIONS = [
-    json.dumps(
+def new_transaction_only_with_id(
+    amount: float,
+    transactionId: str,
+    currency: str,
+) -> NordigenTransaction:
+    return NordigenTransaction(
+        bookingDate="2021-01-01T10:00:00.00000",
+        bookingDatetime="2021-01-01T10:00:00.00000",
+        transactionAmount=TransactionAmount(currency=currency, amount=amount),
+        transactionId=transactionId,
+    )
+
+
+def new_transaction_without_id(amount: float, currency: str) -> NordigenTransaction:
+    return NordigenTransaction(
+        bookingDate="2021-01-01T10:00:00.00000",
+        bookingDatetime="2021-01-01T10:00:00.00000",
+        transactionAmount=TransactionAmount(currency=currency, amount=amount),
+    )
+
+
+def new_transaction_only_with_internal_id(
+    amount: float, internalTransactionId: str, currency: str
+) -> NordigenTransaction:
+    return NordigenTransaction(
+        bookingDate="2021-01-01T10:00:00.00000",
+        bookingDatetime="2021-01-01T10:00:00.00000",
+        transactionAmount=TransactionAmount(currency=currency, amount=amount),
+        internalTransactionId=internalTransactionId,
+    )
+
+
+def create_nordigen_tranctions(transaction_list: List[NordigenTransaction]) -> str:
+    return json.dumps(
         NordigenTransactions(
-            booked=[
-                create_nordigen_transaction(amount=45.00, id="same_id"),
-                create_nordigen_transaction(amount=40.00, internal_id="inter_id"),
-                create_nordigen_transaction(amount=15.00, id="same_id"),
-                create_nordigen_transaction(amount=10.00, internal_id="inter_id"),
-            ],
-            pending=[create_nordigen_transaction(amount=5.00, id="noise")],
+            booked=transaction_list,
+            pending=[new_transaction_only_with_id(5.0, "noise", "EUR")],
         )
-    ),
-    json.dumps(
-        NordigenTransactions(
-            booked=[
-                create_nordigen_transaction(amount=45.00, id="same_id"),
-                create_nordigen_transaction(amount=40.00, internal_id="inter_id"),
-                create_nordigen_transaction(amount=-15.00, id="same_id"),
-                create_nordigen_transaction(amount=-10.00, internal_id="inter_id"),
-            ],
-            pending=[create_nordigen_transaction(amount=5.00, id="noise")],
-        )
-    ),
-]
-TEST_SAME_ID_MERGED = [
-    # Only the first same_id entry will be considered.
-    # According to the Go cardless API, transactionId should not be repeated
-    create_nordigen_transaction(amount=45.00, id="same_id"),
-    create_nordigen_transaction(amount=40.00, internal_id="inter_id"),
-    create_nordigen_transaction(amount=10.00, internal_id="inter_id"),
-    create_nordigen_transaction(amount=-10.00, internal_id="inter_id"),
+    )
+
+
+def swap_transactions(
+    transaciton_list: List[NordigenTransaction],
+) -> List[NordigenTransaction]:
+
+    for i in range(0, len(transaciton_list) - 1):
+        temp = transaciton_list[i]
+        transaciton_list[i] = transaciton_list[i + 1]
+        transaciton_list[i + 1] = temp
+
+    return transaciton_list
+
+
+SAME_ID_TRANSACTIONS = [
+    # An identical transactionId represents the same
+    # transaction regardless of any other parameter.
+    new_transaction_only_with_id(15.0, "same_id", "EUR"),
+    new_transaction_only_with_id(45.0, "same_id", "EUR"),
+    new_transaction(45.0, "same_id", "inter_id", "EUR"),
+    new_transaction_only_with_id(45.0, "same_id", "EUR"),
 ]
 
-TEST_WITHOUT_ID_TRANSACTIONS = [
-    json.dumps(
-        NordigenTransactions(
-            booked=[
-                create_nordigen_transaction(amount=45.00),
-                create_nordigen_transaction(amount=40.00),
-            ],
-            pending=[create_nordigen_transaction(amount=5.00, id="noise")],
-        )
-    ),
-    json.dumps(
-        NordigenTransactions(
-            booked=[
-                create_nordigen_transaction(amount=-45.00),
-                create_nordigen_transaction(amount=-40.00),
-            ],
-            pending=[create_nordigen_transaction(amount=5.00, id="noise")],
-        )
-    ),
+SAME_INTERNAL_ID_TRANSACTIONS = [
+    new_transaction_only_with_internal_id(45.0, "same_id", "EUR"),
+    new_transaction_only_with_internal_id(40.0, "inter_id", "EUR"),
+    new_transaction_only_with_internal_id(40.0, "inter_id", "USD"),
+    new_transaction_only_with_internal_id(40.0, "inter_id", "BRL"),
+    new_transaction_only_with_internal_id(10.0, "inter_id", "EUR"),
+    new_transaction_only_with_internal_id(10.0, "inter_id", "EUR"),
+    new_transaction_only_with_internal_id(10.0, "inter_id", "EUR"),
+    new_transaction_only_with_internal_id(-10.0, "inter_id", "EUR"),
 ]
-TEST_WITHOUT_ID_MERGED = [
-    create_nordigen_transaction(amount=45.00),
-    create_nordigen_transaction(amount=40.00),
-    create_nordigen_transaction(amount=-45.00),
-    create_nordigen_transaction(amount=-40.00),
+
+
+EXPECTED_SAME_ID_MERGED = [
+    # The sorting prioritizes the transaction with more information available
+    new_transaction(45.0, "same_id", "inter_id", "EUR"),
+    new_transaction_only_with_internal_id(45.0, "same_id", "EUR"),
+    new_transaction_only_with_internal_id(40.0, "inter_id", "EUR"),
+    new_transaction_only_with_internal_id(10.0, "inter_id", "EUR"),
+    new_transaction_only_with_internal_id(-10.0, "inter_id", "EUR"),
+    new_transaction_only_with_internal_id(40.0, "inter_id", "BRL"),
+    new_transaction_only_with_internal_id(40.0, "inter_id", "USD"),
 ]
+
+WITHOUT_ID_TRANSACTIONS = [
+    [
+        new_transaction_without_id(45.0, "USD"),
+        new_transaction_without_id(40.0, "USD"),
+    ],
+    [
+        new_transaction_without_id(45.0, "USD"),
+        new_transaction_without_id(-40.0, "USD"),
+    ],
+]
+
+EXPECTED_WITHOUT_ID_MERGED = [
+    new_transaction_without_id(45.0, "USD"),
+    new_transaction_without_id(40.0, "USD"),
+    new_transaction_without_id(45.0, "USD"),
+    new_transaction_without_id(-40.0, "USD"),
+]
+
+
+def assert_is_list_equal(
+    return_list: List[NordigenTransaction],
+    expected_list: List[NordigenTransaction],
+) -> None:
+    for expected_item in expected_list:
+        assert expected_item in return_list
+    assert len(expected_list) == len(return_list)
 
 
 @pytest.mark.parametrize(
-    "transactions_name_list, transactions_list, merged_expected",
+    "transactions_file_names, valid_files_number, transactions_list, merged_expected",
     [
-        ([], [], []),
+        ([], 0, [], []),
         (
-            ["transactions_a.json", "noise.jpg", "transactions_b.json"],
-            TEST_SAME_ID_TRANSACTIONS,
-            TEST_SAME_ID_MERGED,
+            ["transactions_a.json", "transnoise1.bmp", "transactions_b.json"],
+            2,
+            [
+                create_nordigen_tranctions(SAME_ID_TRANSACTIONS),
+                create_nordigen_tranctions(SAME_INTERNAL_ID_TRANSACTIONS),
+            ],
+            EXPECTED_SAME_ID_MERGED,
         ),
         (
-            ["transactions_a.json", "transnoise.json", "transactions_b.json"],
-            TEST_WITHOUT_ID_TRANSACTIONS,
-            TEST_WITHOUT_ID_MERGED,
+            ["transactions_a.json", "transnoise2.jpg", "transactions_b.json"],
+            2,
+            [
+                create_nordigen_tranctions(swap_transactions(SAME_ID_TRANSACTIONS)),
+                create_nordigen_tranctions(
+                    swap_transactions(SAME_INTERNAL_ID_TRANSACTIONS)
+                ),
+            ],
+            EXPECTED_SAME_ID_MERGED,
+        ),
+        (
+            ["transactions_a.json", "transnoise3.json", "transactions_b.json"],
+            2,
+            [
+                create_nordigen_tranctions(WITHOUT_ID_TRANSACTIONS[0]),
+                create_nordigen_tranctions(WITHOUT_ID_TRANSACTIONS[1]),
+            ],
+            EXPECTED_WITHOUT_ID_MERGED,
         ),
     ],
 )
 def test_merge_transaction(
-    transactions_name_list: List[str],
+    transactions_file_names: List[str],
+    valid_files_number: int,
     transactions_list: List[str],
-    merged_expected: Union[str, None],
+    merged_expected: List[NordigenTransaction],
     open_mock: Mock,
     listdir_mock: Mock,
     write_json_mock: Mock,
+    datetime_mock: Mock,
 ) -> None:
 
-    listdir_mock.return_value = transactions_name_list
+    listdir_mock.return_value = transactions_file_names
     open_mock.return_value.read.side_effect = transactions_list
+    mock_dummy_datetime = "2024-01-01T10:00:00.00000"
+    datetime_mock.now.return_value.isoformat.return_value = mock_dummy_datetime
 
     runner = CliRunner()
     result = runner.invoke(merge_transactions, [])
 
     assert result.exit_code == 0
-    assert write_json_mock.call_args.args[1] == merged_expected
+    assert (
+        write_json_mock.call_args.args[0]
+        == f"data/merged_transactions-{mock_dummy_datetime}.json"
+    )
+
+    assert_is_list_equal(
+        return_list=write_json_mock.call_args.args[1], expected_list=merged_expected
+    )
+    assert open_mock.return_value.read.call_count == valid_files_number
 
 
 def test_invalid_transaction_json_input(
@@ -163,7 +249,7 @@ def test_invalid_transaction_json_input(
     write_json_mock.assert_not_called()
 
 
-def test_data_path_not_found(listdir_mock: Mock, write_json_mock: Mock) -> None:
+def test_non_existing_data_folder(listdir_mock: Mock, write_json_mock: Mock) -> None:
     listdir_mock.side_effect = FileNotFoundError
 
     runner = CliRunner()
@@ -175,10 +261,15 @@ def test_data_path_not_found(listdir_mock: Mock, write_json_mock: Mock) -> None:
 
 
 def test_empty_data_folder_should_create_empty_output(
-    open_mock: Mock, listdir_mock: Mock, write_json_mock: Mock
+    open_mock: Mock,
+    listdir_mock: Mock,
+    write_json_mock: Mock,
+    datetime_mock: Mock,
 ) -> None:
 
     listdir_mock.return_value = []
+    mock_dummy_datetime = "2024-01-01T10:00:00.00000"
+    datetime_mock.now.return_value.isoformat.return_value = mock_dummy_datetime
 
     runner = CliRunner()
     result = runner.invoke(merge_transactions, [])
@@ -186,3 +277,7 @@ def test_empty_data_folder_should_create_empty_output(
     open_mock.return_value.read.assert_not_called()
     assert result.exit_code == 0
     assert write_json_mock.call_args.args[1] == []
+    assert (
+        write_json_mock.call_args.args[0]
+        == f"data/merged_transactions-{mock_dummy_datetime}.json"
+    )
