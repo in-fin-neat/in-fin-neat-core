1414cc35c53f77f6433bc8a298cab9900494074b
Leonardo Castilho <leonardohscastilho@gmail.com>
2024-04-25 01:32:56 +0200

    Add a testcase to check output files in generate reports unit test

--- tests/test_generate_reports.py
+++ tests/test_generate_reports.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from datetime import datetime
 import json
 from dateutil.tz import tzutc
@@ -11,9 +12,22 @@ from personal_finances.generate_reports import (
     InvalidDatatimeRange,
     InvalidDatatime,
 )
-from personal_finances.config import clear_user_configuration_cache
-from typing import Generator, Any, List
-from personal_finances.transaction.type import get_income_transactions, get_expense_transactions, get_unknown_type_transactions
+from personal_finances.config import (
+    cache_user_configuration,
+    clear_user_configuration_cache,
+)
+from typing import Dict, Generator, Any, List
+from personal_finances.transaction.categorizing import get_category
+from personal_finances.transaction.grouping import (
+    GroupedTransaction,
+    TransactionGroupingType,
+    group_transactions,
+)
+from personal_finances.transaction.type import (
+    get_income_transactions,
+    get_expense_transactions,
+    get_unknown_type_transactions,
+)
 from personal_finances.transaction.definition import SimpleTransaction
 
 transactions_file = '{"test" : "teste" }'
@@ -29,39 +43,7 @@ def open_mock() -> Generator[Mock, None, None]:
 @fixture()
 def cache_user_configuration_mock() -> Generator[Mock, None, None]:
     with patch("personal_finances.generate_reports.cache_user_configuration") as m:
-        
         yield m
-        
-        
-@pytest.fixture()
-def user_config_mock() -> Generator[Mock, None, None]:
-    with patch(
-        "personal_finances.transaction.categorizing.get_user_configuration"
-    ) as u_mock:
-        _get_expense_category_references.cache_clear(),
-        _get_expense_category_tags.cache_clear()
-        _get_income_category_references.cache_clear()
-        u_mock.return_value.ExpenseCategoryDefinition = [
-            CategoryDefinition(
-                CategoryName="house",
-                CategoryReferences=["ikea", "dealz"],
-                CategoryTags=[],
-            ),
-            CategoryDefinition(
-                CategoryName="restaurants/pubs",
-                CategoryReferences=["some-fancy-pub"],
-                CategoryTags=["#coffee", "#restaurant"],
-            ),
-            CategoryDefinition(
-                CategoryName="entertainment",
-                CategoryReferences=["odeon"],
-                CategoryTags=[],
-            ),
-        ]
-
-        u_mock.return_value.IncomeCategoryDefinition = []
-        yield u_mock
-
 
 
 @fixture()
@@ -75,7 +57,16 @@ def write_json_mock() -> Generator[Mock, None, None]:
     with patch("personal_finances.generate_reports.write_json") as m:
         yield m
 
-        
+
+def assert_is_list_equal(
+    return_list: List[Any],
+    expected_list: List[Any],
+) -> None:
+    for expected_item in expected_list:
+        assert expected_item in return_list
+    assert len(expected_list) == len(return_list)
+
+
 @pytest.mark.parametrize(
     "command_params,exception_type",
     [
@@ -162,20 +153,9 @@ def test_correct_cli_params(
         assert result.exit_code == 0
         cache_user_configuration_mock.assert_called_once_with(user_file_path)
         open_mock.assert_called_once_with(transactions_file_path, "r")
-'''
-@patch("personal_finances.generate_reports.remove_internal_transfers")
-@patch("personal_finances.generate_reports.transaction_datetime_filter")
-@patch("personal_finances.generate_reports.get_income_transactions")
-@patch("personal_finances.generate_reports.get_expense_transactions")
-@patch("personal_finances.generate_reports.get_unknown_type_transactions")
-def test_dummy_output_reports_have_correct_format(
-    get_unknown_type_transactions:Mock,
-    get_expense_transactions:Mock,
-    get_income_transactions:Mock,
-    write_json_mock: Mock) -> None: 
-'''
 
-def _open_transaction_test_file(transactions_file_path) -> List[SimpleTransaction]:
+
+def _open_transaction_test_file(transactions_file_path: str) -> List[SimpleTransaction]:
     with open(transactions_file_path, "r") as transactions_file:
         return list(
             map(
@@ -183,53 +163,195 @@ def _open_transaction_test_file(transactions_file_path) -> List[SimpleTransactio
                 json.loads(transactions_file.read()),
             )
         )
-        
-        
-def _split_transactions(transactions: List[SimpleTransaction]):
-    
-    keys_to_filter = ['f8ca9991a65f4230d108d47514696580', '4dd99d3dd2a66105f441459561e5abcc']
-    transactions = [
-        transaction 
-        for transaction in transactions 
-        if transaction['transactionId'] not in keys_to_filter
+
+
+def _split_transactions(
+    transactions: List[SimpleTransaction],
+) -> tuple[List[SimpleTransaction], List[SimpleTransaction]]:
+
+    keys_to_filter = [
+        "f8ca9991a65f4230d108d47514696580",
+        "4dd99d3dd2a66105f441459561e5abcc",
     ]
-    
+    transactions = [
+        transaction
+        for transaction in transactions
+        if transaction["transactionId"] not in keys_to_filter
+    ]
+
     income_transactions = get_income_transactions(transactions)
-    expense_transactions = get_expense_transactions(transactions) + get_unknown_type_transactions(transactions)
+    expense_transactions = get_expense_transactions(
+        transactions
+    ) + get_unknown_type_transactions(transactions)
     return (income_transactions, expense_transactions)
-    
-    
-def _sum_amount_transactions(transactions: List[SimpleTransaction]) -> int:
-    total_amount = 0
+
+
+def _sum_amount_transactions(transactions: List[SimpleTransaction]) -> float:
+    total_amount = 0.0
     for transaction in transactions:
-        total_amount += transaction["amount"]    
+        total_amount += transaction["amount"]
     return total_amount
 
 
-def test_balance_output_is_correct(write_json_mock: Mock) -> None:    
-
-    transactions_test_file_path = "tests/test_data/transactions/test_transaction.json"
+def _run_generate_reports(transactions_path: str, configuration_path: str) -> int:
     runner = CliRunner()
-    result = runner.invoke(generate_reports, ["-ucfp", 
-                                              "tests/test_data/config/test_config_file.yaml", 
-                                              "-tfp", 
-                                              transactions_test_file_path])
-    
+    return runner.invoke(
+        generate_reports,
+        [
+            "-ucfp",
+            configuration_path,
+            "-tfp",
+            transactions_path,
+        ],
+    ).exit_code
 
-    transactions_raw = _open_transaction_test_file(transactions_test_file_path)
-    income_transactions, expense_transactions = _split_transactions(transactions_raw)
-    
-    total_income = _sum_amount_transactions(income_transactions)
-    total_expense = _sum_amount_transactions(expense_transactions)
+
+def _expected_balance(
+    incomes: List[SimpleTransaction], expenses: List[SimpleTransaction]
+) -> dict:
+    total_income = _sum_amount_transactions(incomes)
+    total_expense = _sum_amount_transactions(expenses)
     total_balance = total_income + total_expense
-    
+
     expected_balance_dict = {
         "start_time": datetime(1970, 1, 1, 0, 0, tzinfo=tzutc()),
         "end_time": datetime(2100, 1, 1, 0, 0, tzinfo=tzutc()),
         "total_income": total_income,
         "total_expense": total_expense,
-        "total_balance": total_balance
+        "total_balance": total_balance,
     }
-    assert write_json_mock.call_args_list[0].args[0] == 'reports/1970-01-01T00:00:00+00:00_2100-01-01T00:00:00+00:00/balance.json'
-    assert write_json_mock.call_args_list[0].args[1] == expected_balance_dict
 
+    return expected_balance_dict
+
+
+def _expected_group_by_ref(grouped_transactions: List[GroupedTransaction]) -> List[Any]:
+    grouped_by_ref: Dict[Any, Any] = {}
+
+    for transaction in grouped_transactions:
+        group_number = transaction["groupNumber"]
+        grouped = grouped_by_ref.get(group_number)
+        if grouped is None:
+            grouped_by_ref[group_number] = {
+                "key": group_number,
+                "amount": transaction["amount"],
+                "references": [transaction["referenceText"]],
+                "groupName": transaction["groupName"],
+            }
+        else:
+            grouped["amount"] += transaction["amount"]
+            grouped["references"] += [transaction["referenceText"]]
+
+    grouped_by_ref_list = list(grouped_by_ref.values())
+    return grouped_by_ref_list
+
+
+def _expected_categ_group(
+    grouped_transactions: List[GroupedTransaction],
+    group_references: List[set[str]],
+) -> List[Any]:
+    transactions = deepcopy(grouped_transactions)
+    transactions_ref = deepcopy(group_references)
+    categories_group: Dict[Any, Any] = {}
+
+    for transaction in transactions:
+        key_category = get_category(
+            transaction["referenceText"],
+            transactions_ref[transaction["groupNumber"]],
+            fallback_reference=transaction["groupName"],
+        )
+        category_group = categories_group.get(key_category)
+        if category_group is None:
+            categories_group[key_category] = {
+                "key": key_category,
+                "amount": transaction["amount"],
+                "references": [transaction["referenceText"]],
+            }
+        else:
+            category_group["amount"] += transaction["amount"]
+            category_group["references"] += [transaction["referenceText"]]
+
+    categories_group_list = list(categories_group.values())
+    return categories_group_list
+
+
+def _expected_categ_transactions(
+    grouped_transactions: List[GroupedTransaction],
+    group_references: List[set[str]],
+) -> List[Any]:
+
+    categorized_transactions: List[Any] = deepcopy(grouped_transactions)
+    categorized_transactions_ref = deepcopy(group_references)
+
+    for transaction in categorized_transactions:
+        key_category = get_category(
+            transaction["referenceText"],
+            categorized_transactions_ref[transaction["groupNumber"]],
+            fallback_reference=transaction["groupName"],
+        )
+        transaction["customCategory"] = key_category
+
+    return categorized_transactions
+
+
+def test_generate_reports_output(write_json_mock: Mock) -> None:
+
+    transactions_test_file_path = "tests/test_data/transactions/test_transaction.json"
+    configuration_test_file_path = "tests/test_data/config/test_config_file.yaml"
+    cache_user_configuration(configuration_test_file_path)
+
+    transactions_raw = _open_transaction_test_file(transactions_test_file_path)
+    incomes, expenses = _split_transactions(transactions_raw)
+    grouped_income, grouped_income_ref = group_transactions(
+        incomes, TransactionGroupingType.ReferenceSimilarity
+    )
+    grouped_expense, grouped_expense_ref = group_transactions(
+        expenses, TransactionGroupingType.ReferenceSimilarity
+    )
+
+    clear_user_configuration_cache()
+
+    exit_code = _run_generate_reports(
+        transactions_test_file_path, configuration_test_file_path
+    )
+    assert exit_code == 0
+
+    result_calls = write_json_mock.call_args_list
+
+    expected_path_name = "reports/1970-01-01T00:00:00+00:00_2100-01-01T00:00:00+00:00/"
+
+    expected_balance_dict = _expected_balance(incomes, expenses)
+
+    ei_group_by_ref = _expected_group_by_ref(grouped_income)
+    ei_categories_group = _expected_categ_group(grouped_income, grouped_income_ref)
+    ei_categorized = _expected_categ_transactions(grouped_income, grouped_income_ref)
+
+    ee_group_by_ref = _expected_group_by_ref(grouped_expense)
+    ee_categories_group = _expected_categ_group(grouped_expense, grouped_expense_ref)
+    ee_categorized = _expected_categ_transactions(grouped_expense, grouped_expense_ref)
+
+    assert result_calls[0].args[0] == expected_path_name + "balance.json"
+    assert result_calls[0].args[1] == expected_balance_dict
+
+    assert result_calls[1].args[0] == expected_path_name + "income_per_group.json"
+    assert_is_list_equal(result_calls[1].args[1], ei_group_by_ref)
+
+    assert result_calls[2].args[0] == expected_path_name + "income_per_category.json"
+    assert_is_list_equal(result_calls[2].args[1], ei_categories_group)
+
+    assert (
+        result_calls[3].args[0]
+        == expected_path_name + "income_categorized_transactions.json"
+    )
+    assert_is_list_equal(result_calls[3].args[1], ei_categorized)
+
+    assert result_calls[4].args[0] == expected_path_name + "expense_per_group.json"
+    assert_is_list_equal(result_calls[4].args[1], ee_group_by_ref)
+
+    assert result_calls[5].args[0] == expected_path_name + "expense_per_category.json"
+    assert_is_list_equal(result_calls[5].args[1], ee_categories_group)
+
+    assert (
+        result_calls[6].args[0]
+        == expected_path_name + "expense_categorized_transactions.json"
+    )
+    assert_is_list_equal(result_calls[6].args[1], ee_categorized)
