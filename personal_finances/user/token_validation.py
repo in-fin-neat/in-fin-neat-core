aa5847da6168ec1fbbba0750807e6bac426e981e
Leonardo Castilho <leonardohscastilho@gmail.com>
2024-07-14 21:26:57 +0200

    Code formatting

--- personal_finances/user/token_validation.py
+++ personal_finances/user/token_validation.py
@@ -1,6 +1,6 @@
-import base64
 import json
 import logging
+from typing import Any
 import boto3
 import jwt
 import os
@@ -8,65 +8,46 @@ import os
 LOGGER = logging.getLogger(__name__)
 
 
-class InvalidInput(Exception):
-    def __str__(self) -> str:
-        return "InvalidInput"
-
+class InvalidLambdaEventInput(Exception):
+    pass
 
 class AuthHeaderNotFound(Exception):
-    def __str__(self) -> str:
-        return "AuthHeaderNotFound"
-
-
-class UserNotFound(Exception):
-    def __str__(self) -> str:
-        return "UserNotFound"
+    pass
 
 
 class EmptyUserID(Exception):
-    def __str__(self) -> str:
-        return "EmptyUserID"
+    pass
 
 
-class TokenNotFound(Exception):
-    def __str__(self) -> str:
-        return "TokenNotFound"
+class AuthorizationHeaderEmptyContent(Exception):
+    pass
 
 
-def _get_token_from_event(event: dict):
+def _get_token_from_event(event: dict) -> Any:
     if "authorizationToken" not in event:
-        raise(AuthHeaderNotFound)
-    
+        raise (AuthHeaderNotFound)
+
     token = event.get("authorizationToken", None)
     if token is None or token == "":
-        raise(TokenNotFound)
-    
+        raise (AuthorizationHeaderEmptyContent)
+
     return token
 
 
 def _get_jwt_secret() -> str:
     jwt_session = boto3.client("secretsmanager")
+    secret_id = os.environ["INFINEAT_JWT_SECRET_NAME"]
 
-    get_secret_value_response = jwt_session.get_secret_value(
-        SecretId=os.environ["INFINEAT_JWT_SECRET_NAME"]
-    )
+    get_secret_value_response = jwt_session.get_secret_value(SecretId=secret_id)
 
     return str(get_secret_value_response["SecretString"])
 
 
-def decode_token(token):
-    return jwt.decode(
-        jwt=token,
-        key=_get_jwt_secret(),
-        algorithms="HS256",
-    )
-
-
-def _generatePolicy(principalId, effect, resource):
-    authResponse = {}
+def _generatePolicy(principalId: str, effect: str, resource: str) -> str:
+    authResponse: dict = {}
     authResponse["principalId"] = principalId
     if effect and resource:
-        policyDocument = {}
+        policyDocument: dict = {}
         policyDocument["Statement"] = []
         statementOne = {}
         statementOne["Action"] = "execute-api:Invoke"
@@ -77,22 +58,26 @@ def _generatePolicy(principalId, effect, resource):
     authResponse_JSON = json.dumps(authResponse)
     return authResponse_JSON
 
-def _get_user_from_decoded_token(token:dict) -> str:
-    if "userId" not in token:
-        raise(UserNotFound)
-    
-    user_id = token.get("userId", None)
+
+def _get_user_from_decoded_token(token: dict) -> str:
+    user_id: str = token.get("userId", None)
     if user_id is None or user_id == "":
-        raise(EmptyUserID)
-    
+        raise (EmptyUserID)
+
     return user_id
 
-def token_validation(event, context):
-    if event is None or event == "":
-        raise(InvalidInput)
-    
+
+def token_validation(event: dict, context: str) -> Any:
+    if event is None or event == {}:
+        raise (InvalidLambdaEventInput)
+
     token = _get_token_from_event(event)
-    decoded_token = jwt.decode(jwt=token, key=_get_jwt_secret(), algorithms="HS256", options={"require": ["exp", "userId"]})
+    decoded_token = jwt.decode(
+        jwt=token,
+        key=_get_jwt_secret(),
+        algorithms=["HS256"],
+        options={"require": ["exp", "userId"]},
+    )
     user_id = _get_user_from_decoded_token(decoded_token)
     response = _generatePolicy(user_id, "Allow", event["methodArn"])
 
