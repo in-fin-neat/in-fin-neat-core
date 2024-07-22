a9c52b83f4451099d771c59b536c536bd9b723f5
Leonardo Castilho <leonardohscastilho@gmail.com>
2024-07-22 20:57:25 +0200

    Simplify user auth exception map and add unit tests

--- personal_finances/user/user_auth_exceptions.py
+++ personal_finances/user/user_auth_exceptions.py
@@ -1,100 +1,118 @@
-from typing import List
+from typing import Dict, Tuple, Type
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 
 class AuthorizationHeaderNotPresent(Exception):
-    def __str__(self) -> str:
-        return "AuthorizationHeaderNotPresent"
+    pass
 
 
 class AuthorizationHeaderEmptyContent(Exception):
-    def __str__(self) -> str:
-        return "AuthorizationHeaderEmptyContent"
+    pass
 
 
 class InvalidAuthorizationHeader(Exception):
-    def __str__(self) -> str:
-        return "InvalidAuthorizationHeader"
+    pass
 
 
 class InvalidLambdaEventInput(Exception):
-    def __str__(self) -> str:
-        return "InvalidLambdaEventInput"
+    pass
 
 
 class UserNotFound(Exception):
-    def __str__(self) -> str:
-        return "UserNotFoundException"
+    pass
 
 
 class PasswordNotMatch(Exception):
-    def __str__(self) -> str:
-        return "PasswordNotMatch"
+    pass
 
 
 class InvalidUserPassword(Exception):
-    def __str__(self) -> str:
-        return "InvalidUserPassword"
+    pass
 
 
 class InvalidUserName(Exception):
-    def __str__(self) -> str:
-        return "InvalidUserName"
-
-
-class ServerSecretNotFound(Exception):
-    def __str__(self) -> str:
-        return "InvalidUserName"
+    pass
 
 
 class InvalidDynamoResponse(Exception):
-    def __str__(self) -> str:
-        return "InvalidUserName"
+    pass
 
 
-_EXPCEPTION_TO_HTTP_RESPONSE_DICT: List[dict] = [
-    {
+class InconsistentExceptionDictionary(Exception):
+    pass
+
+
+"""
+    This dicts contains the map between the exceptions generated in
+    the user login api and the http response. No exception should be
+    duplicated, otherwise the 'InconsistentExceptionDictionary' exception
+    will be raised in the '_get_exception_type_index' function
+"""
+_EXCEPTION_TO_HTTP_RESPONSE: Dict[Tuple, Dict] = {
+    (UserNotFound, PasswordNotMatch): {
         "statusCode": 401,
         "body": "User or password incorrect",
-        "exceptions": [
-            UserNotFound,
-            PasswordNotMatch,
-        ],
     },
-    {
+    (
+        AuthorizationHeaderNotPresent,
+        AuthorizationHeaderEmptyContent,
+        InvalidAuthorizationHeader,
+    ): {
         "statusCode": 400,
         "body": "Invalid authentication input",
-        "exceptions": [
-            AuthorizationHeaderNotPresent,
-            AuthorizationHeaderEmptyContent,
-            InvalidAuthorizationHeader,
-        ],
     },
-    {
+    (
+        InvalidUserPassword,
+        InvalidUserName,
+    ): {
         "statusCode": 400,
         "body": "Username or password does not match the minimal security requirements",
-        "exceptions": [
-            InvalidUserPassword,
-            InvalidUserName,
-        ],
     },
-    {
+    (
+        InvalidLambdaEventInput,
+        InvalidDynamoResponse,
+        KeyError,
+    ): {
         "statusCode": 500,
         "body": "Internal Server Error",
-        "exceptions": [
-            InvalidLambdaEventInput,
-            ServerSecretNotFound,
-            InvalidDynamoResponse,
-        ],
     },
-]
+}
+
+_UNKNOWN_EXCEPTION_RESPONSE: Dict = {
+    "statusCode": 500,
+    "body": "Internal Server Error",
+}
 
 
-def get_server_response_by_exception(exception: Exception) -> dict:
-    for http_response in _EXPCEPTION_TO_HTTP_RESPONSE_DICT:
-        exception_list = http_response["exceptions"]
-        if type(exception) in exception_list:
-            return {
-                "statusCode": http_response["statusCode"],
-                "body": http_response["body"],
-            }
-    return {}
+def _get_exception_type_index(
+    exception_to_http_response: Dict[Tuple, Dict]
+) -> Dict[Type[Exception], Dict]:
+    exception_type_index: Dict[Type[Exception], Dict] = {}
+    for exception_types, http_response in exception_to_http_response.items():
+        if any(
+            exception_type in exception_type_index for exception_type in exception_types
+        ):
+            LOGGER.warning(
+                f"""overriding response for one
+                     or more of the exception type {exception_types}"""
+            )
+            # There should be tests ensuring this is never reached in a shipped code.
+            raise InconsistentExceptionDictionary
+        exception_type_index = {
+            **exception_type_index,
+            **{exception_type: http_response for exception_type in exception_types},
+        }
+    return exception_type_index
+
+
+def get_server_response_by_exception(
+    exception: Exception,
+    exception_list: Dict[Tuple, Dict] = _EXCEPTION_TO_HTTP_RESPONSE,
+) -> dict:
+    exception_type_index = _get_exception_type_index(exception_list)
+    try:
+        return exception_type_index[type(exception)]
+    except KeyError:
+        return _UNKNOWN_EXCEPTION_RESPONSE
